86e121d0b0099ecebff13fffb0784d0c78e9aa14
msabeni <kemimsabeni@gmail.com>
2020-11-17 09:25:27 +0300

    revert last change

--- pages/specialists.js
+++ pages/specialists.js
@@ -1,35 +1,33 @@
-// import ContactListItem from "../components/ContactListItem";
-// import { connectToDatabase } from "../util/mongodb";
+import ContactListItem from "../components/ContactListItem";
+import { connectToDatabase } from "../util/mongodb";
 
-// export default function Specialists({ specialists }) {
-export default function Specialists() {
+export default function Specialists({ specialists }) {
   return (
     <div>
       <h1> Meet Our Specialists!</h1>
       
       <div>
-          Testing
-        {/* {specialists.map((specialist) => (
+        {specialists.map((specialist) => (
            <ContactListItem {...specialist} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
 }
 
-// export async function getServerSideProps() {
-//   const { db } = await connectToDatabase();
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
 
-//   const specialists = await db
-//     .collection("specialists")
-//     .find({})
-//     .sort({ last_name: 1 })
-//     .limit(20)
-//     .toArray();
+  const specialists = await db
+    .collection("specialists")
+    .find({})
+    .sort({ last_name: 1 })
+    .limit(20)
+    .toArray();
 
-//   return {
-//     props: {
-//       specialists: JSON.parse(JSON.stringify(specialists)),
-//     },
-//   };
-// }
+  return {
+    props: {
+      specialists: JSON.parse(JSON.stringify(specialists)),
+    },
+  };
+}
