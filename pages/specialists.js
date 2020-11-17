d5348f6ef8f3f09764579dc0a093f867cd4404a5
msabeni <kemimsabeni@gmail.com>
2020-11-17 09:09:42 +0300

    Statically test rendering

--- pages/specialists.js
+++ pages/specialists.js
@@ -1,33 +1,35 @@
-import ContactListItem from "../components/ContactListItem";
-import { connectToDatabase } from "../util/mongodb";
+// import ContactListItem from "../components/ContactListItem";
+// import { connectToDatabase } from "../util/mongodb";
 
-export default function Specialists({ specialists }) {
+// export default function Specialists({ specialists }) {
+export default function Specialists() {
   return (
     <div>
       <h1> Meet Our Specialists!</h1>
       
       <div>
-        {specialists.map((specialist) => (
+          Testing
+        {/* {specialists.map((specialist) => (
            <ContactListItem {...specialist} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const { db } = await connectToDatabase();
+// export async function getServerSideProps() {
+//   const { db } = await connectToDatabase();
 
-  const specialists = await db
-    .collection("specialists")
-    .find({})
-    .sort({ last_name: 1 })
-    .limit(20)
-    .toArray();
+//   const specialists = await db
+//     .collection("specialists")
+//     .find({})
+//     .sort({ last_name: 1 })
+//     .limit(20)
+//     .toArray();
 
-  return {
-    props: {
-      specialists: JSON.parse(JSON.stringify(specialists)),
-    },
-  };
-}
+//   return {
+//     props: {
+//       specialists: JSON.parse(JSON.stringify(specialists)),
+//     },
+//   };
+// }
